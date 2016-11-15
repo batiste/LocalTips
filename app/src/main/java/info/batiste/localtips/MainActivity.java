d5a2f0fa54c9a5cd2faf49a4effed72ad5c8ecca
Batiste Bieler <batiste.bieler@gmail.com>
2016-11-15 18:45:28 +0100

    feat() add another activity

--- app/src/main/java/info/batiste/localtips/MainActivity.java
+++ app/src/main/java/info/batiste/localtips/MainActivity.java
@@ -56,36 +56,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton takephoto = (FloatingActionButton) findViewById(R.id.takephoto);
+        FloatingActionButton takephoto = (FloatingActionButton) findViewById(R.id.newtip);
         takephoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dispatchTakePictureIntent();
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+                Intent i = new Intent(getBaseContext(), NewTipActivity.class);
+                startActivity(i);
             }
         });
 
-
-        MapFragment map = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        map.getMapAsync(this);
-
-        // Use GPS location data
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_GPS);
-
-        } else {
-            // Acquire a reference to the system Location Manager
-            Log.d("onCreate", "requestLocationUpdates");
-            LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
     }
 
     public final int PERMISSIONS_REQUEST_GPS = 18;
@@ -93,27 +72,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_GPS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // Acquire a reference to the system Location Manager
-                    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-                    Log.d("onRequestPermissions", "requestLocationUpdates");
-
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
     }
 
     @Override
@@ -136,197 +94,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    // Geoloc stuff
-    // Define a listener that responds to location updates
-    LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            // Called when a new location is found by the network location provider.
-            //makeUseOfNewLocation(location);
-            Log.d("onLocationChanged", location.toString());
-
-            if (map != null) {
-                Log.d("onLocationChanged", "Map ready");
-                LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 14.0f));
-            }
-
-
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Log.d("Location", Integer.toString(status));
-            Log.d("Location", extras.toString());
-        }
-
-        public void onProviderEnabled(String provider) {
-            Log.d("onProviderEnabled", "1");
-        }
-
-        public void onProviderDisabled(String provider) {
-            Log.d("onProviderDisabled", "1");
-        }
-    };
-
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    File currentPhoto;
-    Uri photoURI;
-
-    private void dispatchTakePictureIntent() {
-        Log.d("PictureIntent", "start");
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-                currentPhoto = photoFile;
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-                Log.e("PictureIntent", "IOERROR when creating the image file" + ex.toString());
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                photoURI = FileProvider.getUriForFile(this,
-                        "info.batiste.localtips.fileprovider",
-                        photoFile);
-                Log.e("PictureIntent", "photoURI: " + photoURI);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
-            } else {
-                Log.e("PictureIntent", "Image not created");
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            ImageView mImageView = (ImageView) findViewById(R.id.imageview);
-            if (data != null) {
-                // The default Android camera application returns a non-null intent
-                // only when passing back a thumbnail in the returned Intent.
-                // If you pass EXTRA_OUTPUT with a URI to write to, it will return a null intent
-                // and the picture is in the URI that you passed in.
-                Bundle extras = data.getExtras();
-                Bitmap imageBitmap = (Bitmap) extras.get("data");
-                mImageView.setImageBitmap(imageBitmap);
-            }
-
-            if (photoURI != null) {
-                //mImageView.setImageBitmap(Bitmap.createBitmap(photoURI));
-                Log.d("mImageView.setImageURI", photoURI.toString());
-                mImageView.setImageURI(photoURI);
-            }
-
-            if (currentPhoto != null) {
-                setPic(currentPhoto);
-            }
-        }
-    }
-
-    String mCurrentPhotoPath;
-
-    private void setPic(File photoFile) {
-        // Get the dimensions of the View
-        ImageView mImageView = (ImageView) findViewById(R.id.imageview);
-
-        int targetW = mImageView.getWidth();
-        int targetH = mImageView.getHeight();
-
-        Log.d("setPic", "image view W:" + targetW);
-        Log.d("setPic", "image view H:" + targetH);
-
-        // Get the dimensions of the bitmap
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-
-        if (photoFile.exists())
-            Log.d("setPic", "Image exist");
-        else {
-            Log.e("setPic", "Image doesn't exist");
-            return;
-        }
-
-        String path = photoFile.getAbsolutePath();
-        Log.e("setPic", path);
-
-        BitmapFactory.decodeFile(path, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        Log.d("setPic", "photoW:" + photoW);
-        Log.d("setPic", "photoH:" + photoH);
-
-        // Determine how much to scale down the image
-        int scaleFactor = targetW / photoW;
-
-        // Decode the image file into a Bitmap sized to fill the View
-        bmOptions.inJustDecodeBounds = false;
-        //bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);
-
-        int activityHeight = this.getWindow().getDecorView().getHeight();
-        int height = (int)Math.max(activityHeight / 3.5, 250);
-
-        Bitmap newbitMap = scaleCropToFit(bitmap, targetW, height);
-
-        mImageView.setImageBitmap(newbitMap);
-    }
-
-    public static Bitmap scaleCropToFit(Bitmap original, int targetWidth, int targetHeight){
-        //Need to scale the image, keeping the aspect ration first
-        int width = original.getWidth();
-        int height = original.getHeight();
-
-        float widthScale = (float) targetWidth / (float) width;
-        float heightScale = (float) targetHeight / (float) height;
-        float scaledWidth;
-        float scaledHeight;
-
-        int startY = 0;
-        int startX = 0;
-
-        if (widthScale > heightScale) {
-            scaledWidth = targetWidth;
-            scaledHeight = height * widthScale;
-            //crop height by...
-            startY = (int) ((scaledHeight - targetHeight) / 2);
-        } else {
-            scaledHeight = targetHeight;
-            scaledWidth = width * heightScale;
-            //crop width by..
-            startX = (int) ((scaledWidth - targetWidth) / 2);
-        }
-
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(original, (int) scaledWidth, (int) scaledHeight, true);
-
-        Bitmap resizedBitmap = Bitmap.createBitmap(scaledBitmap, startX, startY, targetWidth, targetHeight);
-        return resizedBitmap;
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        if (!image.mkdirs()) {
-            Log.e("createImageFile", "Directory not created");
-        }
-
-        return image;
     }
 
 }
